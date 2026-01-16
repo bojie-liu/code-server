5817fbc1e3d7f87cd95579bcf2e23dec8474995e
joshliu <bojieliu711@gmail.com>
2026-01-16 23:07:26 +0800

    Pull template from git instead of assets

--- src/browser/frameworks/vite-vanilla/main.js
+++
@@ -1,8 +0,0 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <div class="container">
-    <h1>Hello Vite!</h1>
-    <p>Edit main.js to get started.</p>
-  </div>
-`
--- src/browser/templates/basic-html/script.js
+++
@@ -1,3 +0,0 @@
-console.log('Hello from script.js!');
-
-// Your JavaScript code here
